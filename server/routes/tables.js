c4ad230062aa65e5094f45208bc7c5c09760b612
Dmitry <dzmitry.dulevich@itechart-group.com>
2019-01-29 15:06:02 +0300

    Added server for tables

--- server/routes/tables.js
+++ server/routes/tables.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const tables = require('./tables.json');
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    res.send(tables);
 });
 
 module.exports = router;
