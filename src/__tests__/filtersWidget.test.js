8bc3c349a67233b69dfb707da4494325a3621647
Dmitry <dzmitry.dulevich@itechart-group.com>
2019-02-07 14:54:41 +0300

    Removed unused tests and webpack.

--- src/__tests__/filtersWidget.test.js
+++ src/__tests__/filtersWidget.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Menu } from 'react-select/lib/components';
 
 import Context from '../models/context';
 import Dimension from '../models/dimension';
 
 import { FiltersWidget } from '../components/filtersWidget';
+import { FilterComponent } from '../components/filterComponent';
 
 describe('FiltersWidget', () => {  
     const fieldsOptions = ['a', 'b', 'c'];
@@ -37,4 +39,16 @@ describe('FiltersWidget', () => {
         hiddenComponent = component.find('.filtersWidget__body--hidden');
         expect(hiddenComponent.length).toBe(0);
     });
+
+    // This test can't be run due to inner enzyme bugs
+
+    // it('should emit necessary onChange events by click', () => {
+
+    //     let contextSelect = component.find(FilterComponent).filter({ selectName:'contextsOptions' });
+    //     contextSelect.find('div.filtersWidget__dropdown-indicator').simulate('mouseDown', { button: 0 });
+    //     console.log(contextSelect.html());
+    //     const contextSelectMenu = contextSelect.find('.option');
+    //     console.log(contextSelectMenu.html());
+
+    // })
 });
